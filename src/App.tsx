a2bf9704d33288de509e36815d169ba45e69ccdd
Christopher MASSAMBA <christopher.massamba@cegelem.fr>
2026-06-15 17:08:12 +0200

    App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,39 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { AuthProvider } from "./contexts/AuthContext"
+import { PrivateRoute } from "./components/PrivateRoute"
+import { Login } from "./pages/Login"
+import { Dashboard } from "./pages/Dashboard"
+import { Profile } from "./pages/Profile"
 function App() {
-  return <div>Auth Dashboard</div>
+  return (
+      <BrowserRouter>
+        <AuthProvider>
+          <Routes>
+            <Route path="/login" element={<Login/>}/>
+
+            <Route
+              path="/dashboard"
+              element={
+                <PrivateRoute>
+                  <Dashboard/>
+                </PrivateRoute>
+              }
+            />
+
+            <Route
+                path="/profile"
+                element={
+                  <PrivateRoute>
+                    <Profile />
+                  </PrivateRoute>
+                }
+            />
+
+            <Route path="*" element={<Navigate to="/login" replace />} />
+          </Routes>
+        </AuthProvider>
+      </BrowserRouter>
+  )
 }
 
 export default App
