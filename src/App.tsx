28895b87dfaa65e2ceab05302e378624f6d72414
Christopher MASSAMBA <christopher.massamba@cegelem.fr>
2026-06-15 14:25:26 +0200

    initialCommit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div>Auth Dashboard</div>
+}
+
+export default App
---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,32 @@
+import { createContext, useState } from 'react'
+import type { ReactNode } from 'react'
+import type { User, AuthContextType } from '../types/auth'
+
+export const AuthContext = createContext<AuthContextType | null>(null)
+
+
+export function AuthProvider({ children }: { children: ReactNode }) {
+    const [user, setUser] = useState<User | null>(null)
+
+    function login(email: string, _password: string) {
+
+        const fakeUser: User = {
+            id:1,
+            name: 'Christopher',
+            email: email,
+        }
+        setUser(fakeUser)
+        localStorage.setItem('user', JSON.stringify(fakeUser))
+    }
+
+    function logout() {
+        setUser(null)
+        localStorage.removeItem('user')
+    }
+
+    return (
+        <AuthContext.Provider value={{user, login, logout}}>
+            { children }
+        </AuthContext.Provider>
+    )
+}
