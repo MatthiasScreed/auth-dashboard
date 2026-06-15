3ced5be9a7028cddc922f2fd8a4c2c31b20a793a
Christopher MASSAMBA <christopher.massamba@cegelem.fr>
2026-06-15 14:33:12 +0200

    Ajout useAuth

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,13 @@
+import { useContext } from 'react'
+import  { AuthContext } from '../contexts/AuthContext'
+import type {AuthContextType} from "../types/auth.ts";
+
+export function useAuth() : AuthContextType {
+    const context = useContext(AuthContext)
+
+    if(context === null) {
+        throw new Error('useAuth must be used within an AuthProvider')
+    }
+
+    return context
+}
